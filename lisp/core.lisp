9bd622be8d5180eb029859aac378abf3d02962d7
MatiasGabriel <matiasgabrielcarp@gmail.com>
2026-06-11 13:13:21 -0300

    Análisis y Recomendación de Ciclos

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -37,6 +37,7 @@
       ((< resto 90)  'en-rojo)
       ((< resto 210) 'en-verde)
       (t  'en-amarillo))))
+
 ;; ========================================================
 ;; REQUERIMIENTO 3: Sistema de Auditoría
 ;; FUNCION: auditoria-cambio
@@ -47,3 +48,26 @@
 (defun auditoria-cambio (tiempo color-anterior color-nuevo)
   ;; Utiliza FORMAT para la salida estándar por pantalla
   (format t "Tiempo A: la luz ha cambiado de ~A a ~A%" tiempo color-anterior color-nuevo))
+
+;; ========================================================
+;; REQUERIMIENTO 4a: Análisis de Ciclos (Duración)
+;; FUNCION: duracion-ciclo
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Composición
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun duracion-ciclo (t-rojo t-amarillo t-verde)
+  (+ t-rojo t-amarillo t-verde))
+
+;; ========================================================
+;; REQUERIMIENTO 4b: Recomendación de Ciclos
+;; FUNCION: recomendacion-ciclo
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Condicional
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun recomendacion-ciclo (duracion)
+  (cond
+    ((< duracion 35) "El ciclo es demasiado corto. Dificil acomodacion psicologica.")
+    ((> duracion 150) "El ciclo es demasiado largo. Dificil acomodacion psicologica.")
+    (t "El ciclo esta dentro del rango optimo (35-150 segundos).")))
